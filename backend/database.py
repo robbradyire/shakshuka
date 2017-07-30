7d9ef7761c168795ba1f1685f3e8ad8146b4d92e
Robert Brady <robbradyire@gmail.com>
2017-07-30 19:39:33 +0100

    Same as previous commit

--- backend/database.py
+++ backend/database.py
@@ -1,142 +1,191 @@
 # database.py
 # contains the logic for interacting with the database
 
+import environ
+import os
 import sqlite3
 
-###################################
-# GET
-###################################
+class RecipeDatabase:
+    """Stores database interaction methods"""
 
-def get_recipes():
-    """Return all recipe ids and names
+    def __init__(self, test=True):
+        """Creates a RecipeDatabase
+        
+        Params:
+            test: chooses whether to use the test or production db
+        returns: RecipeDatabase
+        """
+        if test:
+            self.db = os.environ["TEST_DB"]
+        else:
+            self.db = os.environ["RECIPE_DB"]
 
-    Params: None
-    Return: list
-    """
-    with sqlite3.connect("recipes.db") as connection:
-        cursor = connection.cursor()
-        cursor.execute("SELECT id, name FROM recipe")
-        return cursor.fetchall()
-    return []
+    def new_db(self):
+        """Creates a new database if there isn't one present already
+        
+        Params: None
+        Returns: True or False
+        """
+        with sqlite3.connect(self.db) as connection:
+            cursor = connection.cursor()
+            with open('sql/create_tables.sql') as f:
+                contents = f.read()
+                cursor.executescript(contents)
+            connection.commit()
+            return True
+        return False
 
-def get_recipe(recipe_id):
-    """Return a recipe by its ID
-    
-    Params:
-        recipe_id: int
-    Return: recipe OR None
-    """
-    with sqlite3.connect("recipes.db") as connection:
-        cursor = connection.cursor()
-        query = ' '.join((
-            "SELECT *",
-            "FROM recipes WHERE id=?",
-        ))
-        cursor.execute(query, (recipe_id,))
-        return cursor.fetchone()
-    return None
+    def drop_db(self):
+        """Clear the database
+        
+        Params: None
+        Return: True or False
+        """
+        with sqlite3.connect(self.db) as connection:
+            cursor = connection.cursor()
+            table_names = ["recipe","ingredient","recipe_ingredient",
+                "tag", "recipe_tag"
+            ] 
+            for name in table_names:
+                cursor.execute("DELETE FROM {}".format(name))
+            connection.commit()
+            return True
+        return False
 
-###################################
-# CREATE
-###################################
+    ###################################
+    # GET
+    ###################################
 
-def create_recipe(args):
-    """Create a recipe
-    
-    Params:
-        args: dict
-    Return: True or None
-    """
-    with sqlite3.connect("recipes.db") as connection:
-        cursor = connection.cursor()
-        query = ' '.join((
-            "INSERT INTO recipes",
-            "(name, instructions, link, comments, servings)",
-            "VALUES (?,?,?,?,?)",
-        ))
-        cursor.execute(query,
-            (args["name"], args["instructions"], args["link"], 
-             args["comments"], args["servings"],))
-        cursor.commit()
-        return True
-    return None
+    def get_recipes(self):
+        """Return all recipe ids and names
 
-def create_recipe_ingredient(args):
-    """Create a recipe ingredient
-    
-    Params:
-        args: dict
-    Return: True or None
-    """
-    with sqlite3.connect("recipes.db") as connection:
-        cursor = connection.cursor()
-        query = ' '.join((
-            "INSERT INTO recipe_ingredient",
-            "(recipe_id, ingredient_id, preparation, quantity)",
-            "VALUES (?,?,?,?)",
-        ))
-        cursor.execute(query,
-            (args["recipe_id"], args["ingredient_id"], args["preparation"], 
-             args["quantity"],))
-        cursor.commit()
-        return True
-    return None
+        Params: None
+        Return: list
+        """
+        with sqlite3.connect(self.db) as connection:
+            cursor = connection.cursor()
+            cursor.execute("SELECT id, name FROM recipe")
+            return cursor.fetchall()
+        return []
 
-def create_recipe_tag(args):
-    return None
+    def get_recipe(self, recipe_id):
+        """Return a recipe by its ID
+        
+        Params:
+            recipe_id: int
+        Return: recipe OR None
+        """
+        with sqlite3.connect(self.db) as connection:
+            cursor = connection.cursor()
+            query = ' '.join((
+                "SELECT *",
+                "FROM recipes WHERE id=?",
+            ))
+            cursor.execute(query, (recipe_id,))
+            return cursor.fetchone()
+        return None
 
-def create_ingredient(args):
-    return None
+    ###################################
+    # CREATE
+    ###################################
 
-def create_tag(args):
-    return None
+    def create_recipe(self, args):
+        """Create a recipe
+        
+        Params:
+            args: dict
+        Return: True or None
+        """
+        with sqlite3.connect(self.db) as connection:
+            cursor = connection.cursor()
+            query = ' '.join((
+                "INSERT INTO recipes",
+                "(name, instructions, link, comments, servings)",
+                "VALUES (?,?,?,?,?)",
+            ))
+            cursor.execute(query,
+                (args["name"], args["instructions"], args["link"], 
+                 args["comments"], args["servings"],))
+            connection.commit()
+            return True
+        return None
 
-###################################
-# UPDATE
-###################################
+    def create_recipe_ingredient(self, args):
+        """Create a recipe ingredient
+        
+        Params:
+            args: dict
+        Return: True or None
+        """
+        with sqlite3.connect(self.db) as connection:
+            cursor = connection.cursor()
+            query = ' '.join((
+                "INSERT INTO recipe_ingredient",
+                "(recipe_id, ingredient_id, preparation, quantity)",
+                "VALUES (?,?,?,?)",
+            ))
+            cursor.execute(query,
+                (args["recipe_id"], args["ingredient_id"], args["preparation"], 
+                 args["quantity"],))
+            connection.commit()
+            return True
+        return None
 
-def update_recipe(recipe_id, args):
-    return None
+    def create_recipe_tag(self, args):
+        return None
 
-def update_recipe_ingredient(recipe_id, ingredient_id, args):
-    return None
+    def create_ingredient(self, args):
+        return None
 
-def update_recipe_tag(recipe_id, ingredient_id, args):
-    return None
+    def create_tag(self, args):
+        return None
 
-def update_ingredient(recipe_id, args):
-    return None
+    ###################################
+    # UPDATE
+    ###################################
 
-def update_tag(recipe_id, tag_id, args):
-    return None
+    def update_recipe(self, recipe_id, args):
+        return None
 
-###################################
-# DELETE
-###################################
+    def update_recipe_ingredient(self, recipe_id, ingredient_id, args):
+        return None
 
-def delete_recipe(recipe_id):
-    """Delete a recipe by its ID
+    def update_recipe_tag(self, recipe_id, ingredient_id, args):
+        return None
 
-    Params:
-        recipe_id: int
-    Return: recipe or None
-    """
-    with sqlite3.connect("recipes.db") as connection:
-        cursor = connection.cursor()
-        cursor.execute("SQL")
-        cursor.commit()
-        return cursor.fetchone()
-    return None
+    def update_ingredient(self, recipe_id, args):
+        return None
 
-def delete_recipe_ingredient(recipe_id, ingredient_id, args):
-    return None
+    def update_tag(self, recipe_id, tag_id, args):
+        return None
 
-def delete_recipe_tag(recipe_id, ingredient_id, args):
-    return None
+    ###################################
+    # DELETE
+    ###################################
 
-def delete_ingredient(recipe_id, args):
-    return None
+    def delete_recipe(self, recipe_id):
+        """Delete a recipe by its ID
 
-def delete_tag(recipe_id, tag_id, args):
-    return None
+        Params:
+            recipe_id: int
+        Return: recipe or None
+        """
+        with sqlite3.connect(self.db) as connection:
+            cursor = connection.cursor()
+            cursor.execute("SQL")
+            connection.commit()
+            return cursor.fetchone()
+        return None
+
+    def delete_recipe_ingredient(self, recipe_id, ingredient_id, args):
+        return None
+
+    def delete_recipe_tag(self, recipe_id, ingredient_id, args):
+        return None
+
+    def delete_ingredient(self, recipe_id, args):
+        return None
+
+    def delete_tag(self, recipe_id, tag_id, args):
+        return None
 
---
+++ backend/sample_environ.py
@@ -0,0 +1,9 @@
+# sample_environ.py
+# Sample environment variables for the project
+# 
+
+import os
+
+os.environ["RECIPE_DB"] = "my_db.db"
+os.environ["TEST_DB"] = "my_test_db.db"
+
